0072f1116909e55c5c38dfa37c75dc9b4c3de444
Oleg Grigoriev <go.vasac@gmail.com>
2015-11-27 09:52:14 +0300

    io\Test: Htpasswd file mock for tests

---
+++ io/Test.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package axy\htpasswd
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+
+namespace axy\htpasswd\io;
+
+/**
+ * Htpasswd file mock for tests
+ */
+class Test implements IFile
+{
+    /**
+     * The constructor
+     *
+     * @param string|string[] $content [optional]
+     */
+    public function __construct($content = null)
+    {
+        if ($content === null) {
+            $content = '';
+        } elseif (is_array($content)) {
+            $content = implode(PHP_EOL, $content);
+        }
+        $this->content = $content;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load()
+    {
+        return $this->content;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Returns the content as an array of lines
+     */
+    public function getLines()
+    {
+        return explode("\n", $this->content);
+    }
+
+    /**
+     * @var string
+     */
+    private $content;
+}
---
+++ tests/io/TestTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package axy\htpasswd
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+
+namespace axy\htpasswd\tests\io;
+
+use axy\htpasswd\io\Test;
+
+/**
+ * coversDefaultClass axy\htpasswd\io\Test
+ */
+class TestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * covers ::load
+     * covers ::save
+     */
+    public function testLoadSave()
+    {
+        $test = new Test();
+        $this->assertSame('', $test->load());
+        $test->save('One'.PHP_EOL.'Two'.PHP_EOL.'Three');
+        $this->assertSame('One'.PHP_EOL.'Two'.PHP_EOL.'Three', $test->load());
+        $this->assertSame(['One', 'Two', 'Three'], $test->getLines());
+        $test->save('X');
+        $this->assertSame('X', $test->load());
+        $this->assertSame(['X'], $test->getLines());
+    }
+
+    /**
+     * covers ::load
+     */
+    public function testConstructString()
+    {
+        $test = new Test('One'.PHP_EOL.'Two');
+        $this->assertSame('One'.PHP_EOL.'Two', $test->load());
+        $this->assertSame(['One', 'Two'], $test->getLines());
+    }
+
+
+    /**
+     * covers ::load
+     */
+    public function testConstructArray()
+    {
+        $test = new Test(['One', 'Two']);
+        $this->assertSame('One'.PHP_EOL.'Two', $test->load());
+        $this->assertSame(['One', 'Two'], $test->getLines());
+    }
+}
